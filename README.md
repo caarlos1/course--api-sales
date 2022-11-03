aa73b99ee1f73ad815d23de0ed502dee3999bb6a
Carlos Roberto <carlos.junior@dotgroup.com.br>
2022-11-03 20:28:39 -0300

    config course project part 2

---
+++ README.md
@@ -0,0 +1,3 @@
+## Criando container Postgres
+
+    docker run --name postgres -e POSTGRES_PASSWORD=docker -p 65432:5432 -d postgres
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,34 @@
-// console.log('Hello World');
+import 'reflect-metadata'; // necessário para o typeorm
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm'; // Criando a conexão do TypeORM
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+// Tratamento de erros (middleware)
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response
+        .status(error.statusCode)
+        .json({ status: 'error', message: error.message });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Inernal server error',
+    });
+  },
+);
+
+app.listen(3333, () => {
+  console.log('Server on 3333');
+});
